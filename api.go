d22f77fb60763348238fcc365aa3f657447e5fd6
xiangjiaoflb <xiangjiaoflb@gmail.com>
2019-06-26 21:40:42 +0800

    第一次提交

---
+++ api.go
@@ -0,0 +1,51 @@
+package requesthandle
+
+import (
+	"net"
+	"net/http"
+	"time"
+)
+
+func newHHTTPClient() *http.Client {
+	client := &http.Client{Transport: &http.Transport{
+		Dial: func(netw, addr string) (net.Conn, error) {
+			c, err := net.DialTimeout(netw, addr, time.Second*5) //设置建立连接超时
+			if err != nil {
+				return nil, err
+			}
+			c.SetDeadline(time.Now().Add(6 * time.Second)) //设置发送接收数据超时
+			return c, nil
+		},
+	}}
+	return client
+}
+
+// AllRequest ...
+func AllRequest(getReq func() (*http.Request, error), callback func(resp *http.Response) (bool, error)) error {
+	for {
+		//创建http请求
+		req, err := getReq()
+		if err != nil {
+			return err
+		}
+
+		client := newHHTTPClient()
+		resp, err := client.Do(req)
+		if err != nil {
+			return err
+		}
+
+		defer resp.Body.Close()
+		//处理返回结果
+		ok, err := callback(resp)
+		if ok {
+			time.Sleep(time.Second)
+			continue
+		}
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/xiangjiaoflb/requesthandle
+
+go 1.12
